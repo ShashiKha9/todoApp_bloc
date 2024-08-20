8c7cb65b3a3f7671dfbde42fa5baae6725f679d8
shashikha9 <87931813+ShashiKha9@users.noreply.github.com>
2024-08-20 11:34:56 +0530

    hive completed

---
+++ lib/todo/data/model/todoModel.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todoModel.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TodoAdapter extends TypeAdapter<Todo> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Todo read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Todo(
+      title: fields[1] as String,
+      isCompleted: fields[2] as bool?,
+      id: fields[0] as String?,
+      isDeleted: fields[3] as bool?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Todo obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.isCompleted)
+      ..writeByte(3)
+      ..write(obj.isDeleted);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TodoAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
